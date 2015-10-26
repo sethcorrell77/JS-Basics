6d7dad3e9fa397061d46d113fa5eb04f2345b8fc
sethcorrell77 <sethcorrell77@gmail.com>
2015-10-25 23:55:37 -0500

    finished day 5

--- closures.js
+++ closures.js
@@ -186,20 +186,20 @@ function myFunc(){
           return function() {
             console.log(j);
           }
-        }(j))
+        }(i))
       )
    }
    return myArray;
- }
+}
 
 var funcArray = myFunc();
 
-  funcArray[0](); //0
-  funcArray[1](); //1
-  funcArray[2](); //2
-  funcArray[3](); //3
-  funcArray[4](); //4
-  funcArray[5](); //5
+funcArray[0](); //0
+funcArray[1](); //1
+funcArray[2](); //2
+funcArray[3](); //3
+funcArray[4](); //4
+funcArray[5](); //5
 
 
 
